ef1ab8fdf23dcd97a64e7b7a2275e974b3197d3e
Eunhak0401 <74fresh4490@gmail.com>
2024-04-27 17:13:05 +0900

    1단계
    점수 1점부터 10점까지 나타내기

---
+++ Bowling.py
@@ -0,0 +1,33 @@
+s = 10
+p = 10
+
+score = int(input('점수를 입력하시오:'))
+
+if 1 <= score <= 10:
+    print(score)
+
+
+'''
+if score == 10:
+        score = 10
+elif score == 9:
+        score = 9
+elif score == 8:
+        score = 8
+elif score == 7:
+        score = 7
+elif score == 6:
+        score = 6
+elif score == 5:
+        score = 5
+elif score == 4:
+        score = 4
+elif score == 3:
+        score = 3
+elif score == 2:
+        score = 2
+elif score == 1:
+        score = 1
+        print(score)
+
+'''
