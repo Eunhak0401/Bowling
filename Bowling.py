0657a3c47fa58a5cace0b5435026e7540fa47b24
Eunhak0401 <74fresh4490@gmail.com>
2024-05-30 12:38:12 +0900

    5/30 볼링문제

--- Bowling.py
+++ Bowling.py
@@ -1,51 +1,42 @@
 if __name__ == '__main__':
+ #점수를 입력받아 출력하는 방식이다. 문제는 점수를 몇개를 입력할지도 모른다.
+ '''
+ def Enter_score():
+         s = "s"
+         p = "p"
+         m = "m"
+         while True:
+                 score = int(input("점수를 입력하시오: "))
+                 if 1 <= score <= 10:
+                         print(score)
+                         return score
+                 elif score == s:
+                         print("10")
+                         return s
+                 elif score == p:
+                         print("10")
+                         return p
+                 elif score == m:
+                         print("0")
+                         return m
+                 elif score >= 11:
+                         print("다시 입력하세요")
+                 else:
+                         print("다시 입력하세요")
 
-        s = 10
-        p = 10
-        m = 0
-        socre = int(m)
-        score = int(s)
-        score = int(p)
-
-while True:
-        socre = int(input("점수를 입력하시오: "))
-        if 1 <= score <= 10:
-                print(score)
-        elif score == s:
-                print(socre)
-        elif score == p :
-                print(score)
-        elif score == m:
-                print(score)
-        elif score >= 11:
-                print("다시 입력하세요")
-                break
-
+frame = Enter_score()
+set1 = 10 * frame
 '''
-for i in range(1,21):
-        for i in range(1,2):
-                if (score == s):
-                        score = input('')
 
-if score == 10:
-        score = 10
-elif score == 9:
-        score = 9
-elif score == 8:
-        score = 8
-elif score == 7:
-        score = 7
-elif score == 6:
-        score = 6
-elif score == 5:
-        score = 5
-elif score == 4:
-        score = 4
-elif score == 3:
-        score = 3
-elif score == 2:
-        score = 2
-elif score == 1:
-        score = 1
-        print(score)
-'''
+
+ # 함수 안에 리스트 생성 후 그 안에 점수를 넣는 방식
+ def game():
+         score[]
+
+
+
+
+
+
+
+
