69a34343379c147bedd0c1c6210202de8b5f5603
Eunhak0401 <74fresh4490@gmail.com>
2024-07-01 00:50:30 +0900

    게음을 총 10프레임까지 진행되도록 만듬

--- Bowling.py
+++ Bowling.py
@@ -1,38 +1,47 @@
+def game():
+    score = []
+
+    print('점수 입력:', end=' ')
+    user_input = input().strip()
+
+    if user_input.lower() == 'q':
+        return score
+
+    temp_list = []
+    for char in user_input:
+        if char == 'S':
+            temp_list.append(10)
+        elif char == 'P':
+            temp_list.append(10)
+        elif char == '-':
+            temp_list.append(0)
+        else:
+            try:
+                bowling_score = int(char)
+                if 1 <= bowling_score <= 10:
+                    temp_list.append(bowling_score)
+                else:
+                    print(f"점수 {bowling_score}은(는) 1에서 10 사이여야 합니다.")
+            except ValueError:
+                print(f"유효하지 않은 입력: {char}")
+
+    # 2개씩 끊어서 리스트에 추가
+    for i in range(0, len(temp_list), 2):
+        score.append(temp_list[i:i + 2])
+        # 10프레임까지
+        if len(score) >= 10:
+            break
+
+    # 현재 점수 합계 계산
+    total_score = sum(sum(pair) for pair in score)
+    print(f"점수: {total_score}")
+
+    return score
+
+
 if __name__ == '__main__':
-
- # 함수 안에 리스트 생성 후 그 안에 점수를 넣는 방식
- def game():
-     score = []
-     while True:
-         print('점수를 입력하세요:', end=' ')
-         user_input = input().strip()
-
-         for char in user_input:
-             if char == 'S':
-                 score.append(10)
-             elif char == 'P':
-                 score.append(10)
-             elif char == '-':
-                 score.append(0)
-             else:
-                 try:
-                     bowling_score = int(char)
-                     if 1 <= bowling_score <= 10:
-                         score.append(bowling_score)
-                     else:
-                         print(f"점수 {bowling_score}은(는) 1에서 10 사이여야 합니다.")
-                 except ValueError:
-                     print(f"유효하지 않은 입력: {char}")
-
-         result = sum(score)
-         print(result)
-
-     return score
-
-
- print(game())
-
-
+    final_score = game()
+    total_final_score = sum(sum(pair) for pair in final_score)
 
 
 
